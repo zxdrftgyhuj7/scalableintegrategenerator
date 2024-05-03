f6ebcc9f2b422fb684a90580354d305bcebb7dde
zxdrftgyhuj7 <gs1lolzkiki@hotmail.com>
2024-05-04 02:32:10 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const fruits = ["apple", "banana", "orange"];
-for (const fruit of fruits) {
-  console.log(fruit);
+function maxEnvelopes(envelopes) {
+  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+  const dp = new Array(envelopes.length).fill(1);
+  for (let i = 0; i < envelopes.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (envelopes[i][1] > envelopes[j][1]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return Math.max(...dp);
 }
