81fb863aad14d540ff9c850e0a91f9a7058ebe1f
zxdrftgyhuj7 <gs1lolzkiki@hotmail.com>
2024-05-04 10:27:25 +0700

    feat project

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-function maxEnvelopes(envelopes) {
-  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
-  const dp = new Array(envelopes.length).fill(1);
-  for (let i = 0; i < envelopes.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (envelopes[i][1] > envelopes[j][1]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-      }
-    }
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
   }
-  return Math.max(...dp);
+  return -1;
 }
